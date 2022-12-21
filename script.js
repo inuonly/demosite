2a2469a50e2d43e779b7097b2b4842066b2e9b3d
K2 <inuonly@gmail.com>
2022-12-22 02:41:39 +0530

    check async function working

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function demo(){
     alert("hi! \n Alert script works for now!");
 };
+
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+  
+  // some code (try to change x to 5)
+  
+    if (x == 0) {
+      myResolve("OK");
+    } else {
+      myReject("Error");
+    }
+  });
+  
+  myPromise.then(
+    function(value) {myDisplayer(value);},
+    function(error) {myDisplayer(error);}
+  );
