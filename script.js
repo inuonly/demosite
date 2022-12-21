dab133f03859569f8eb5b7deac1fcb5bd479ad46
K2 <inuonly@gmail.com>
2022-12-22 03:08:55 +0530

    testing promise using icon file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function myDisplayer(some) {
 }
 let myPromise = new Promise(function(myResolve, myReject) {
     let req = new XMLHttpRequest();
-    req.open('GET', "index2.html");
+    req.open('GET', "favicon.ico");
     req.onload = function() {
         if (req.status == 200) {
                 myResolve(req.response);
