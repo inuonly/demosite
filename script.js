41434d9ba2c25a26bf75425a3023f305e627636b
K2 <inuonly@gmail.com>
2022-12-22 05:33:32 +0530

    gpay testing 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function demo(){
     alert("hi! \n Alert script works for now!");
 };
-function myDisplayer(some) {
+/* function myDisplayer(some) {
     document.getElementById("demopara").innerHTML = some;
 }
 let myPromise = new Promise(function(myResolve, myReject) {
@@ -20,4 +20,53 @@ let myPromise = new Promise(function(myResolve, myReject) {
 myPromise.then(
     function(value) {myDisplayer(value);},
     function(error) {myDisplayer(error);}
-);
+); */
+
+/* gpay section code begins */
+
+let googlePayClient;
+const tokenizationSpecification = {
+    type: 'PAYMENT_GATEWAY',
+    parameters: {
+        gateway: 'example',
+        gatewayMerchantId: 'gatewayMerchantID',
+    }
+};
+const cardPaymentMethod = {
+    type: 'CARD',
+    parameters: {
+        allowedCardNetworks: ['VISA','MASTERCARD'],
+        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+    }
+};
+const googlePayConfiguration = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [cardPaymentMethod]
+};
+function onGooglePayLoaded(){
+    googlePayClient = new google.payments.api.PaymentsClient({
+        environment: 'TEST', 
+    });
+
+    googlePayClient.isReadyToPay(googlePayConfiguration)
+    .then(response => {
+        if (response.result){
+            createAndAddButton();
+        }else{
+            //current user cannot pay using google pay offer another payment option here
+        }
+    })
+    .catch(error => console.error('isReadyToPay error : ', error ));   
+}
+
+function createAndAddButton() {
+    const googlePayButton = googlePayClient.createButton({
+        onclick: onGooglePayButtonClicked
+    });
+
+    document.getElementById('buy-now').appendChild(googlePayButton);
+}
+
+
+/* gpay section code ends */
